3df4680915f7c8d45a92750e91605fa1e5912160
Alexey Nesterenko <alexei.ne@gmail.com>
2025-01-14 17:06:38 +0700

    font and new build

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const dev = process.argv.includes('dev');
-console.log('dev:', dev);
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
@@ -15,7 +12,7 @@ const config = {
 			'~': 'src'
 		},
 		paths: {
-			base: dev ? '' : '/acc2/dist'
+			base: process.argv.includes('dev') ? '' : '/acc2/dist'
 		},
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
