0ad04601d12e895c1fb1e22bf4e00b5e4ea6b304
Alexey Nesterenko <alexei.ne@gmail.com>
2025-01-14 17:11:07 +0700

    typo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,11 @@
 import typography from '@tailwindcss/typography';
 import type { Config } from 'tailwindcss';
-import defaultTheme from 'tailwindcss/defaultTheme';
 
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {
-			fontFamily: {
-				sans: ['InterVariable', ...defaultTheme.fontFamily.sans]
-			}
-		}
+		extend: {}
 	},
 
 	plugins: [typography]
